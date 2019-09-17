1ae0d591e1bdccbd40a91eef9928cad3ba06adf9
denverwilliams <denver@debian.nz>
2019-09-17 19:00:02 -0400

    vulk/cncf_ci#227 provision crosscloudci/crosscloudci#155 provision now downloads hypercube and kubeadm binaries and generates a sha hash

---
+++ bin/k8sutils.rb
@@ -0,0 +1,37 @@
+require 'digest/sha1'
+require 'rest-client'
+
+class K8sUtils
+  STABLE_RELEASE_URL="https://storage.googleapis.com/kubernetes-release/release/stable.txt"
+  HEAD_RELEASE_URL="https://storage.googleapis.com/kubernetes-release-dev/ci-cross/latest.txt"
+
+  def self.kubernetes_release(release_type)
+    release_url = case release_type
+                  when "stable"
+                    "#{STABLE_RELEASE_URL}"
+                  when "head"
+                    "#{HEAD_RELEASE_URL}"
+                  else
+                    puts "Release type #{release_type} unknown!"
+                    exit 1
+                  end
+
+    response = Faraday.get release_url
+    if response.body.nil?
+      @logger.error "Failed to download release for #{release_type}"
+      return
+    end
+
+    kubernetes_release = "#{response.body}"
+  end
+
+  def self.k8s_sha(download_url)
+    response = Faraday.get download_url
+    raw = RestClient::Request.execute(
+      method: :get,
+      url: 'http://releases.ubuntu.com/16.04.2/ubuntu-16.04.2-desktop-amd64.iso',
+      log: Logger.new(STDOUT),
+      raw_response: true)
+    Digest::SHA256.file(raw.file.path).hexdigest
+  end
+end
